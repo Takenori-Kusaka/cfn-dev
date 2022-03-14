ac6f4c3c60523afb88637e5d829c6dea2a41800f
Takenori Kusaka <kokorokagami@gmail.com>
2022-03-14 21:34:15 +0900

    Update cfn_dev_stack.py

--- cfn_dev/cfn_dev_stack.py
+++ cfn_dev/cfn_dev_stack.py
@@ -10,10 +10,24 @@ class CfnDevStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # create a cloud9 ec2 environment in a new VPC
+        vpc = ec2.Vpc(self, "VPC", max_azs=3)
+        cloud9.Ec2Environment(self, "Cloud9Env", vpc=vpc)
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CfnDevQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # or create the cloud9 environment in the default VPC with specific instanceType
+        default_vpc = ec2.Vpc.from_lookup(self, "DefaultVPC", is_default=True)
+        cloud9.Ec2Environment(self, "Cloud9Env2",
+            vpc=default_vpc,
+            instance_type=ec2.InstanceType("t2.micro")
+        )
+
+        # or specify in a different subnetSelection
+        c9env = cloud9.Ec2Environment(self, "Cloud9Env3",
+            vpc=vpc,
+            subnet_selection=ec2.SubnetSelection(
+                subnet_type=ec2.SubnetType.PRIVATE
+            )
+        )
+
+        # print the Cloud9 IDE URL in the output
+        CfnOutput(self, "URL", value=c9env.ide_url)
